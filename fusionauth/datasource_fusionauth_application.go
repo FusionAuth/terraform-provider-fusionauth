12eb1e2abb856166b1f132ffdab14c3cfd6e4038
Michael Brandenburg <michael.brandenburg@gpsinsight.com>
2020-09-16 17:31:47 -0700

    feat: Add datasource application

---
+++ fusionauth/datasource_fusionauth_application.go
@@ -0,0 +1,44 @@
+package fusionauth
+
+import (
+	"fmt"
+
+	"github.com/FusionAuth/go-client/pkg/fusionauth"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceApplication() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceApplicationRead,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The name of the Application.",
+			},
+		},
+	}
+}
+
+func dataSourceApplicationRead(data *schema.ResourceData, i interface{}) error {
+	client := i.(Client)
+
+	resp, err := client.FAClient.RetrieveApplications()
+	if err != nil {
+		return err
+	}
+	name := data.Get("name").(string)
+	var app *fusionauth.Application
+
+	for i := range resp.Applications {
+		if resp.Applications[i].Name == name {
+			app = &resp.Applications[i]
+		}
+	}
+	if app == nil {
+		return fmt.Errorf("couldn't find application %s", name)
+	}
+	data.SetId(app.Id)
+	return nil
+}
