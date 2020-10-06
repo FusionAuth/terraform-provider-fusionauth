664bb147a87e128dea4251a4181d80d7c8124185
Michael Brandenburg <michael.brandenburg@gpsinsight.com>
2020-10-06 11:15:17 -0700

    fix: diff suppress func for templates

--- fusionauth/helpers.go
+++ fusionauth/helpers.go
@@ -3,6 +3,7 @@ package fusionauth
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/FusionAuth/go-client/pkg/fusionauth"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -29,4 +30,17 @@ func checkResponse(statusCode int, faErrors *fusionauth.Errors) error {
 	default:
 		return fmt.Errorf("unexpected status code: %d(%s) Errors: %v", statusCode, http.StatusText(statusCode), faErrors)
 	}
+}
+
+func templateCompare(k, old, new string, d *schema.ResourceData) bool {
+	clean := func(s string) string {
+		s = strings.ReplaceAll(s, " ", "")
+		s = strings.ReplaceAll(s, "\t", "")
+		s = strings.ReplaceAll(s, "\r\n", "\n")
+		s = strings.ReplaceAll(s, "\n", "")
+		return s
+	}
+	old = clean(old)
+	new = clean(new)
+	return old == new
 }
