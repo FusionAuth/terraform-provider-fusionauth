8ba879d4f76ada0fee5f9dd0b1ecff8ed2d4a099
Michael Brandenburg <mcbrande@gmail.com>
2020-10-31 17:29:41 -0700

    feat: add form resource

---
+++ docs/resources/form.md
@@ -0,0 +1,30 @@
+# Form Resource
+
+A FusionAuth Form is a customizable object that contains one-to-many ordered steps. Each step is comprised of one or more Form Fields.
+
+[Form API](https://fusionauth.io/docs/v1/tech/apis/forms/)
+
+## Example Useage
+
+```hcl
+resource "fusionauth_form" "form" {
+  data = {
+    "description" : "This form customizes the registration experience."
+  }
+  name = "Custom Registration Form"
+  steps {
+    fields = ["91909721-7d4f-b110-8f21-cfdee2a1edb8"]
+  }
+  steps {
+    fields = ["8ed89a31-c325-3156-72ed-6e89183af917", "a977cfd4-a9ed-c4cf-650f-f4539268ac38"]
+  }
+}
+```
+
+## Argument Reference
+
+* `form_id` - (Optional) The Id to use for the new Form. If not specified a secure random UUID will be generated.
+* `data` - (Optional) An object that can hold any information about the Form Field that should be persisted.
+* `name` - (Required) The unique name of the Form Field.
+* `steps` - (Optional) An ordered list of objects containing one or more Form Fields. A Form must have at least one step defined.
+    - `fields` - (Optional) An ordered list of Form Field Ids assigned to this step.
---
+++ fusionauth/resource_fusionauth_form.go
@@ -0,0 +1,160 @@
+package fusionauth
+
+import (
+	"fmt"
+
+	"github.com/FusionAuth/go-client/pkg/fusionauth"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceForm() *schema.Resource {
+	return &schema.Resource{
+		Create: createForm,
+		Read:   readForm,
+		Update: updateForm,
+		Delete: deleteForm,
+		Schema: map[string]*schema.Schema{
+			"form_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "The Id to use for the new Form. If not specified a secure random UUID will be generated.",
+				ValidateFunc: validation.IsUUID,
+			},
+			"data": {
+				Type:        schema.TypeMap,
+				Optional:    true,
+				Description: "An object that can hold any information about the Form that should be persisted.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The unique name of the Form.",
+			},
+			"steps": {
+				Type:        schema.TypeList,
+				Description: "An ordered list of objects containing one or more Form Fields. A Form must have at least one step defined.",
+				MinItems:    1,
+				Required:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"fields": {
+							Type:        schema.TypeList,
+							Elem:        &schema.Schema{Type: schema.TypeString},
+							MinItems:    1,
+							Required:    true,
+							Description: "An ordered list of Form Field Ids assigned to this step.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func createForm(data *schema.ResourceData, i interface{}) error {
+	client := i.(Client)
+	f := buildForm(data)
+	var fid string
+	if fi, ok := data.GetOk("form_id"); ok {
+		fid = fi.(string)
+	}
+	resp, faErrs, err := client.FAClient.CreateForm(fid, fusionauth.FormRequest{Form: f})
+	if err != nil {
+		return fmt.Errorf("createForm err: %v", err)
+	}
+	if err := checkResponse(resp.StatusCode, faErrs); err != nil {
+		return err
+	}
+	data.SetId(resp.Form.Id)
+	return buildResourceDataFromForm(data, resp.Form)
+}
+
+func readForm(data *schema.ResourceData, i interface{}) error {
+	client := i.(Client)
+	id := data.Id()
+
+	resp, err := client.FAClient.RetrieveForm(id)
+	if err != nil {
+		return err
+	}
+	if err := checkResponse(resp.StatusCode, nil); err != nil {
+		return err
+	}
+
+	return buildResourceDataFromForm(data, resp.Form)
+}
+
+func updateForm(data *schema.ResourceData, i interface{}) error {
+	client := i.(Client)
+	f := buildForm(data)
+
+	resp, faErrs, err := client.FAClient.UpdateForm(data.Id(), fusionauth.FormRequest{Form: f})
+	if err != nil {
+		return fmt.Errorf("updateForm err: %v", err)
+	}
+	if err := checkResponse(resp.StatusCode, faErrs); err != nil {
+		return err
+	}
+	data.SetId(resp.Form.Id)
+	return buildResourceDataFromForm(data, resp.Form)
+}
+
+func deleteForm(data *schema.ResourceData, i interface{}) error {
+	client := i.(Client)
+	id := data.Id()
+
+	resp, faErrs, err := client.FAClient.DeleteForm(id)
+	if err != nil {
+		return err
+	}
+	if err := checkResponse(resp.StatusCode, faErrs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildForm(data *schema.ResourceData) fusionauth.Form {
+	s := data.Get("steps")
+	slice, _ := s.([]interface{})
+	steps := make([]fusionauth.FormStep, 0, len(slice))
+	for _, i := range slice {
+		m := i.(map[string]interface{})
+		fields := m["fields"].([]interface{})
+		var f []string
+		for _, x := range fields {
+			f = append(f, x.(string))
+		}
+		steps = append(steps, fusionauth.FormStep{
+			Fields: f,
+		})
+	}
+
+	return fusionauth.Form{
+		Data:  data.Get("data").(map[string]interface{}),
+		Name:  data.Get("name").(string),
+		Steps: steps,
+	}
+}
+
+func buildResourceDataFromForm(data *schema.ResourceData, f fusionauth.Form) error {
+	if err := data.Set("data", f.Data); err != nil {
+		return fmt.Errorf("form.data: %s", err.Error())
+	}
+	if err := data.Set("name", f.Name); err != nil {
+		return fmt.Errorf("form.name: %s", err.Error())
+	}
+
+	fs := make([]map[string]interface{}, 0, len(f.Steps))
+	for _, step := range f.Steps {
+		fs = append(fs, map[string]interface{}{
+			"fields": step.Fields,
+		},
+		)
+	}
+	if err := data.Set("steps", fs); err != nil {
+		return fmt.Errorf("form.steps: %s", err.Error())
+	}
+	return nil
+}
