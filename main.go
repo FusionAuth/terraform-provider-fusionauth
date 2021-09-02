b2f13465d8fe68eed39176f73b7d3899e0087256
Matthew Hartstonge <matt@mykro.co.nz>
2021-09-02 16:41:52 +1200

    feat: enables support for SDK v2 debugging.
    Refer: https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html#support-for-debuggable-provider-binaries

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/gpsinsight/terraform-provider-fusionauth/fusionauth"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: fusionauth.Provider,
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: fusionauth.Provider}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/gpsinsight/fusionauth", opts)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
