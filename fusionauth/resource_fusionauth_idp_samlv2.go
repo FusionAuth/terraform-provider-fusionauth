3340def2f390455fcff3e1595f4ed80a2031bee1
Stuart Auld <stuart.auld@viostream.com>
2024-04-22 15:41:12 +1000

    Allow key rotations without destroying SAMLv2 IDP resources

--- fusionauth/resource_fusionauth_idp_samlv2.go
+++ fusionauth/resource_fusionauth_idp_samlv2.go
@@ -114,7 +114,6 @@ func resourceIDPSAMLv2() *schema.Resource {
 				Required:     true,
 				ValidateFunc: validation.IsUUID,
 				Description:  "The id of the key stored in Key Master that is used to verify the SAML response sent back to FusionAuth from the identity provider. This key must be a verification only key or certificate (meaning that it only has a public key component).",
-				ForceNew:     true,
 			},
 			"lambda_reconcile_id": {
 				Type:         schema.TypeString,
